50b5e46b09c4537a6cf24aef9cbdec3d4262d921
Chad Taylor <caepo3@gmail.com>
2013-09-07 22:18:06 -0500

    Simplified implementation of formatter

--- lib/so2db/formatter.rb
+++ lib/so2db/formatter.rb
@@ -1,112 +1,42 @@
-#--
-# Copyright (c) 2012 Chad Taylor
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
-# of this software and associated documentation files (the "Software"), to deal 
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in 
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
-# SOFTWARE.
-
-
-require 'active_support/inflector'
 require 'nokogiri'
-require 'cgi'
 
 module SO2DB
 
-  # Formats data from one stream into another stream.
+  # Formats data from an XML text stream into another stream.
   class Formatter
+    attr_reader :delimiter, :attributes
 
     # Infrastructure.  Do not call this from your code.
-    def initialize(path = '', delimiter = 11.chr.to_s, lookup = Models::Lookup.new)
+    def initialize(attributes, delimiter = 11.chr.to_s)
+      @attributes = attributes
       @delimiter = delimiter
-      @path = path
-      @lookup = lookup
       @name = 'row'
     end
 
-    # Formats a file and prints the formatted output to the outstream.
-    #
-    # The output is performed via a 'puts' method.
-    #
-    # Example:
-    #   >> f = get_formatter  # assumed to be provided to you
-    #   >> cmd = get_cmd      # some shell command that accepts piped input
-    #   >> IO.popen(cmd, 'r+') do |s| 
-    #   >>   formatter.format(s)
-    #   >>   s.close_write
-    #   >> end
-    def format(outstream)
-      file = File.basename(@path, '.*')
-      req_attrs = @lookup.get_required_attrs(file)
-
-      format_from_stream(File.open(@path), req_attrs, outstream)
-    end
-
-    def file_name
-      File.basename(@path)
-    end
-
-    # Returns a string containing the data type and individual fields provided
-    # through formatting. The fields are sorted alphabetically.
-    #
-    # This method is useful for building SQL statements for the formatted data.
-    #
-    # Example:
-    #   >> f = get_formatter_for_badges # formatter should be provided to you
-    #   >> puts f.value_str
-    #   => badges(id,date,name,user_id)
-    def value_str
-      file = File.basename(@path, '.*')
-      o = @lookup.find_class(file)
-
-      table = o.table_name
-      values = o.exported_fields.sort.join(",")
-
-      "#{table}(#{values})"
+    def format(instream, outstream)
+      Nokogiri::XML::Reader(instream).each do |node|
+        outstream.puts format_node node if element_start? node
+      end
     end
 
     private
-    def format_from_stream(instream, required_attrs, outstream)
-      reader = Nokogiri::XML::Reader(instream)
-      required_attrs.sort!
-
-      reader.each do |node|
-        outstream.puts format_node(node, required_attrs) if element_start? node
-      end
-    end
 
     def element_start?(node)
       node.name == @name &&
         node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
     end
 
-    def format_node(node, attrs)
-      arr = attrs.map do |a|
+    def format_node(node)
+      arr = attributes.map do |a|
         str = node.attribute(a)
         str ? scrub(str) : ''
       end
 
-      arr.join(@delimiter)
+      arr.join(delimiter)
     end
 
     def scrub(str)
-      s = CGI::escapeHTML(str)
-      s.gsub!(/\n|\r/, '')
-
-      s
+      str.gsub(/\n|\r/, '')
     end
 
   end
--- test/test_formatter.rb
+++ test/test_formatter.rb
@@ -4,139 +4,31 @@ require 'so2db'
 class FormatterTest < MiniTest::Unit::TestCase
 
   def setup
-    @formatter = SO2DB::Formatter.new("/tmp/badges.xml")
+    @delimiter = ','
+    @attributes = [ 'Id', 'UserId', 'Name', 'Text', 'Date', 'SomethingElse' ]
+    @formatter = SO2DB::Formatter.new @attributes, @delimiter
+    @xml = <<-eoxml
+<data>
+<fake-row id="3"/>
+<row Id="1" UserId="2" Name="Autobiographer" Date="2010-07-20T19:07:22.990" Text="asdf" />
+</data>
+    eoxml
   end
 
-  def test_initializer_with_default_args
-    skip
-
-    f = SO2DB::Formatter.new
-    assert_equal '', f.instance_variable_get(:@path)
-    assert_equal 11.chr.to_s, f.instance_variable_get(:@delimiter)
-  end
-
-  def test_initializer_with_provided_args
-    skip
-
-    path = '/my/test/path'
-    delimiter = 12.chr.to_s
-    f = SO2DB::Formatter.new(path, delimiter)
-
-    assert_equal path, f.instance_variable_get(:@path)
-    assert_equal delimiter, f.instance_variable_get(:@delimiter)
+  def test_initialize
+    assert_equal @delimiter, @formatter.delimiter
+    assert_equal @attributes, @formatter.attributes
   end
 
   def test_format
-    skip
-
-    file = "file"
-    outstream = "outstream"
-    attrs = [ :a, :b, :c ]
-    SO2DB::Models::Lookup.expects(:get_required_attrs).with("badges").once.returns(attrs)
-    File.expects(:open).with("/tmp/badges.xml").once.returns(file)
-    @formatter.expects(:format_from_stream).with(file, attrs, outstream).once.returns("x")
-
-    result = @formatter.format(outstream)
-    assert_equal "x", result
-  end
-
-  def test_format_from_stream
-    skip
-
-    x = <<-eoxml
-<data>
-<fake-row id="3"/>
-<row Id="1" UserId="2" Name="Autobiographer" Date="2010-07-20T19:07:22.990" />
-</data>
-    eoxml
-
     r, w = IO.pipe
 
-    arr = [ "Id", "UserId", "Name", "Date", "Missing" ]
-    @formatter.send(:format_from_stream, x, arr, w)
-
-    values = [ '2010-07-20T19:07:22.990', '1', '', 'Autobiographer', '2' ]
-    expected = values.join(11.chr.to_s) << "\n"
+    @formatter.format(@xml, w)
     actual = r.gets
+
+    expected = "1,2,Autobiographer,asdf,2010-07-20T19:07:22.990,\n"
 
     assert_equal expected, actual
   end
 
-  def test_file_name
-    skip
-
-    assert_equal "badges.xml", @formatter.file_name
-  end
-
-  def test_value_str
-    skip
-
-    assert_equal "badges(date,id,name,user_id)", @formatter.value_str
-  end
-
-  def create_node_stub(name, node_type)
-    obj = mock()
-    obj.stubs(:name).returns(name)
-    obj.stubs(:node_type).returns(node_type)
-
-    obj
-  end
-
-  def test_element_start_with_good_values
-    skip
-
-    node = create_node_stub("row", Nokogiri::XML::Reader::TYPE_ELEMENT)
-    assert @formatter.send(:element_start?, node)
-  end
-
-  def test_element_start_with_invalid_type
-    skip
-
-    node = create_node_stub("row", Nokogiri::XML::Reader::TYPE_END_ELEMENT)
-    assert_equal false, @formatter.send(:element_start?, node)
-  end
-
-  def test_element_start_with_invalid_name
-    skip
-
-    node = create_node_stub("badges", Nokogiri::XML::Reader::TYPE_ELEMENT)
-    assert_equal false, @formatter.send(:element_start?, node)
-  end
-
-  def test_format_node
-    skip
-
-    node = mock()
-    node.stubs(:attribute).with("Id").returns("1")
-    node.stubs(:attribute).with("Name").returns("Anony Mous")
-
-    @formatter.expects(:scrub).with("1").once.returns("1")
-    @formatter.expects(:scrub).with("Anony Mous").once.returns("Anony Mous")
-
-    result = @formatter.send(:format_node, node, [ "Id", "Name" ])
-
-    assert_equal "1\vAnony Mous", result
-  end
-
-  def test_format_node_with_missing_attribute
-    skip
-
-    node = mock()
-    node.stubs(:attribute).with("Id").returns("1")
-    node.stubs(:attribute).with("Name").returns(nil)
-
-    @formatter.expects(:scrub).once.with("1").returns("1")
-
-    result = @formatter.send(:format_node, node, [ "Id", "Name" ])
-
-    assert_equal "1\v", result
-  end
-
-  def test_scrub
-    skip
-
-    assert_equal '&lt;asdffdsa&gt;', @formatter.send(:scrub, "<asdf\nfdsa\r>")
-  end
-
 end
-
